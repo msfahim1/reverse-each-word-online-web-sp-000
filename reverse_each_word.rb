cd002cb033ff1408d95780acb5f86cf18e065990
Michael Fahim <michael_fahim@yahoo.com>
2019-11-28 18:21:16 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,4 +1,5 @@
 def reverse_each_word(string)
+  array = string.split(" ")
    test_array = []
   array.collect do|string| #iterate over the array
     test_array << string.reverse #reverse each word in the array
